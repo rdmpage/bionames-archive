4966b0d7c0806a441fe69c056ab51b4b03f56ca0
Roderic Page <rdmpage@gmail.com>
2016-05-18 17:41:39 +0100

    Skip commented out lines

--- from_file.php
+++ from_file.php
@@ -9,6 +9,9 @@ require_once (dirname(__FILE__) . '/pdf.php');
 $filename = 'test.txt';
 $filename = 'hbs.txt';
 //$filename = 'tssa.txt';
+$filename = 'wash.txt';
+
+$filename = 'extra.txt';
 
 $shas=array();
 
@@ -17,56 +20,73 @@ $file_handle = fopen($filename, "r");
 while (!feof($file_handle)) 
 {
 	$line = trim(fgets($file_handle));
-	$parts = explode("\t", $line);
 	
-	$filename = $parts[0];
-	$url = $parts[1];
-
-	echo "filename=$filename\n";
-	echo "url=$url\n";
+	if (preg_match('/^#/', $line))
+	{
+		// skip
+	}
+	else
+	{
+		$parts = explode("\t", $line);
+		
+		$filename = $parts[0];
+		$url = $parts[1];
 	
-	// Do we have this already?
-	$sha1 = pdf_with_url($url);
-	
-	//print_r($sha1);
-	
-	if (!$sha1)
-	{	
-		// nope
+		echo "filename=$filename\n";
+		echo "url=$url\n";
 		
-		$pdf = new stdclass;
-		$pdf->urls = array();
-		$pdf->urls[] = $url;
+		// Do we have this already?
+		$sha1 = pdf_with_url($url);
 		
-		// sha1 is sha1 of local file
-		$pdf->sha1 = sha1($filename);
-		$pdf->_id = $pdf->sha1;
+		//print_r($sha1);
 		
-		$shas[] = $pdf->sha1;
+		echo "sha1='$sha1'\n"; //exit();
 		
-		// Do we have a file with this sha1?
-		$sha1 = pdf_with_sha1($pdf->sha1);
-		if ($sha1)
-		{
-			echo "have\n";
+		if (!$sha1 || ($sha1 == ''))
+		{	
+			// nope
+			
+			$pdf = new stdclass;
+			$pdf->urls = array();
+			$pdf->urls[] = $url;
+			
+			// sha1 is sha1 of local file
+			$pdf->sha1 = sha1($filename);
+			$pdf->_id = $pdf->sha1;
+			
+			echo $pdf->sha1 . "\n";
+			
+			$shas[] = $pdf->sha1;
+			
+			// Do we have a file with this sha1?
+			$sha1 = pdf_with_sha1($pdf->sha1);
+			if ($sha1)
+			{
+				echo "have\n";
+			}
+			else
+			{			
+				// New PDF
+				$pdf->relative_path = sha1_to_path_string($pdf->sha1);
+				$pdf->filepath = create_path_from_sha1($pdf->sha1, $config['pdf_file_root']);
+				$pdf->filename = $pdf->sha1 . '.pdf';
+				
+				// Copy PDF 
+				echo $filename . ' ' .  $pdf->filepath. '/' . $pdf->filename. "\n";
+				copy ($filename, $pdf->filepath. '/' . $pdf->filename);
+							
+				print_r($pdf);
+				
+				// New PDF, so add to database					
+				$resp = $couch->send("POST", "/" . $config['couchdb'], json_encode($pdf));	
+				
+				echo $resp . "\n";
+				
+			}
 		}
 		else
-		{			
-			// New PDF
-			$pdf->relative_path = sha1_to_path_string($pdf->sha1);
-			$pdf->filepath = create_path_from_sha1($pdf->sha1, $config['pdf_file_root']);
-			$pdf->filename = $pdf->sha1 . '.pdf';
-			
-			// Copy PDF 
-			copy ($filename, $pdf->filepath. '/' . $pdf->filename);
-						
-			print_r($pdf);
-			
-			// New PDF, so add to database					
-			$resp = $couch->send("POST", "/" . $config['couchdb'], json_encode($pdf));	
-			
-			echo $resp . "\n";
-			
+		{
+			echo "done already!\n";
 		}
 	}
 }
